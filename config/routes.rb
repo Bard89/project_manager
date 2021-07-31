82977d121fcf7ceed03d8544e417a373fe4bc5e5
Vojtech Matous <vojtech.matous@gmail.com>
2021-07-31 22:48:26 +0200

    minor comments added

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,18 @@ Rails.application.routes.draw do
     resources :projects do # creates the routes for all the crud
       # if I don't need all the crud action, I can pick just some, with only or except
 
+      # from the ADVANCED ROUTING LECTURE CCA 29TH MINUTE
       # collection -> created action in the project rout that's called top 
       # collection do
       #   get :top
       # end
 
-      # member
-  end
+      # member -> will be nested in the projects routes, collection will not
+      # so i nest it but don't need a new model for it ...
+      # member do
+      #   get :chef
+      # end
+  
+    end
+
 end
