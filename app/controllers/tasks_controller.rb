8b4b61d28a2c6c7e9721704837ef6a6c056d10a6
Vojtech Matous <vojtech.matous@gmail.com>
2021-08-04 01:16:14 +0200

    task delete link modified

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -53,7 +53,7 @@ class TasksController < ApplicationController
 
     def destroy
         @task.destroy
-        redirect_to project_path(@task.project)
+        redirect_to project_tasks_path(@task.project)
     end
     
     private
