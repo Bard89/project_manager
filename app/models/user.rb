a397e8c517d3eb98231893e54d123997033667a3
Vojtech Matous <vojtech.matous@gmail.com>
2021-08-04 12:37:07 +0200

    profile delete with deletion of projects and tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
   has_many :projects, dependent: :destroy
-  has_many :tasks #IMP PROB no dependent: :destroy here, because I don't wanna dele the tasks 
+  has_many :tasks, dependent: :destroy #IMP PROB no dependent: :destroy here, because I don't wanna dele the tasks 
+  # or maybe I do, like it's not specifically written in the zadani, but logically tasks shouldn't exist withou a project ... 
+  
     # but still not sure how the many to many works with dependent: :destroy
     # try it out
 end
