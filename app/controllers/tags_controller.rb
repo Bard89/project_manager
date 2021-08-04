ae2fab028b6acabfebff044420b12dbb609f5f43
Vojtech Matous <vojtech.matous@gmail.com>
2021-08-04 20:53:22 +0200

    index pundit for tags added

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,28 @@
 class TagsController < ApplicationController
     def index
-
+        @tags = policy_scope(Tag)
         raise
         # @tasks = policy_scope(Task.where(project_id: @project.id))
     end
 
-    
+    # def new
+    #     @project = Project.new
+    #     authorize @project
+    # end
+
+    # def create
+       
+    #     @project = Project.new(project_params)
+    #     @project.user_id = current_user.id
+    #     authorize @project
+    #     if @project.save
+    #         flash[:success] = "Project successfully created"
+    #         redirect_to project_path(@project)
+    #     else
+    #         flash[:error] = "Something went wrong"
+    #         render 'new' 
+    #     end
+    # end
+
+
 end
--- app/policies/tag_policy.rb
+++ app/policies/tag_policy.rb
@@ -1,7 +1,11 @@
 class TagPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.all
+      if user.admin?
+        scope.all
+      else
+        scope.where(user_id: user).order(position: :asc) # don't have to do user_id: user.id, i guess it takes the id parameter fro mthe user on its own
+      end
     end
   end
 
