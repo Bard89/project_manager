c50e6f36d5d811891ca5a9c4dbc2b7866fa8f6ea
Vojtech Matous <vojtech.matous@gmail.com>
2021-08-08 19:31:29 +0200

    remove file added

--- app/models/task.rb
+++ app/models/task.rb
@@ -19,4 +19,14 @@ class Task < ApplicationRecord
         }
   # multisearch from pg gem
   multisearchable against: [:title]
+  
+  # to be able to delete the uoploaded file in task
+  attr_accessor :state_event, :remove_file
+
+  after_save :purge_file, if: :remove_file
+  private def purge_file
+    file.purge_later
+  end
+
+
 end
