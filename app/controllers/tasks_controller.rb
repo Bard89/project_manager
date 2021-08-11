a58e3fe6a99ef9da7346e2185bb5d81c1ad55574
Vojtech Matous <vojtech.matous@gmail.com>
2021-08-12 00:51:45 +0200

    create and update task better solution fix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -57,7 +57,8 @@ class TasksController < ApplicationController
         @task.project = @project
         @task.user = current_user
         authorize @task
-        if @task.save && @task.update(task_params)
+        
+        if @task.save || params[:task][:tag_ids]
             @task.tag_ids = params[:task][:tag_ids]
             flash[:success] = "Task successfully created"
             redirect_to project_task_path(@project, @task)
@@ -71,7 +72,7 @@ class TasksController < ApplicationController
     end
 
     def update
-        if @task.update(task_params)
+        if @task.update(task_params) || params[:task][:tag_ids]
             @task.tag_ids = params[:task][:tag_ids]
             flash[:success] = "Object was successfully updated"
             redirect_to project_task_path(@task.project, @task)
